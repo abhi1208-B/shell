5d19f77b6991a14735b2136383589889d395763c
abhi1208-B <abhishekhm606@gmail.com>
2025-11-26 11:32:33 +0000

    first

---
+++ harsh.sh
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hii Page</title>
+<style>
+body {
+	  font-family: sans-serif;
+	    display: flex;
+	      justify-content: center;
+	        align-items: center;
+		  height: 100vh;
+		    margin: 0;
+		      background-color: #f0f0f0;
+	      }
+
+      .centered-text {
+	        font-size: 5em;
+		  color: #333;
+	  }
+  </style>
+  </head>
+  <body>
+
+  <h1 class="centered-text">Hii</h1>
+
+  </body>
+  </html>
+
